adc8248844384048886627f6ebe1bc6242da715e
Ondřej "Lango" Langr <langrik666@gmail.com>
2022-02-01 14:01:58 +0100

    TokenAuthorizator: Rewrite internal logic for partial checks. (#7)
    * Token require fix when requireToken false
    
    * bdump removed
    
    * Check in methods.
    
    * PhpStan fix
    
    Co-authored-by: Jan Barášek <janbarasek@gmail.com>

--- src/TokenAuthorizator.php
+++ src/TokenAuthorizator.php
@@ -35,38 +35,14 @@ final class TokenAuthorizator implements MatchExtension
 	 */
 	public function beforeProcess(Endpoint $endpoint, array $params, string $action, string $method): ?Response
 	{
-		if ($this->strategy->isActive() === false) {
+		if (
+			$this->strategy->isActive() === false
+			|| $this->isPublicAccess($endpoint)
+			|| $this->isTokenOk($params['token'] ?? null)
+		) {
 			return null;
 		}
-		$token = $params['token'] ?? null;
-		if ($token === null) {
-			throw new \InvalidArgumentException('Parameter "token" is required.');
-		}
-		if (is_string($token) === false) {
-			throw new \InvalidArgumentException(sprintf('Parameter "token" must be string, but type "%s" given.', get_debug_type($token)));
-		}
-		$requireToken = false;
-		try {
-			$ref = new \ReflectionClass($endpoint);
-			$docComment = trim((string) $ref->getDocComment());
-			if (preg_match('/@public(?:$|\s|\n)/', $docComment) === 1) {
-				return null;
-			}
-			foreach ($ref->getAttributes(PublicEndpoint::class) as $publicEndpointAttribute) {
-				if (($publicEndpointAttribute->getArguments()['requireToken'] ?? false) === true) {
-					$requireToken = true;
-				}
-			}
-		} catch (\ReflectionException $e) {
-			throw new \InvalidArgumentException(
-				sprintf('Endpoint "%s" can not be reflected: %s', $endpoint::class, $e->getMessage()),
-				500,
-				$e,
-			);
-		}
-		if ($requireToken === false || $this->strategy->verify($token)) {
-			return null;
-		}
+
 		throw new \InvalidArgumentException('Token is invalid or expired, please contact your administrator.');
 	}
 
@@ -77,5 +53,35 @@ final class TokenAuthorizator implements MatchExtension
 	public function afterProcess(Endpoint $endpoint, array $params, ?Response $response): ?Response
 	{
 		return null;
+	}
+
+
+	private function isPublicAccess(Endpoint $endpoint): bool
+	{
+		$requireToken = false;
+		$ref = new \ReflectionClass($endpoint);
+		if (str_contains((string) $ref->getDocComment(), '@public')) {
+			return true;
+		}
+		foreach ($ref->getAttributes(PublicEndpoint::class) as $publicEndpointAttribute) {
+			if (($publicEndpointAttribute->getArguments()['requireToken'] ?? false) === true) {
+				$requireToken = true;
+			}
+		}
+
+		return $requireToken === false;
+	}
+
+
+	private function isTokenOk(mixed $token): bool
+	{
+		if ($token === null) {
+			throw new \InvalidArgumentException('Parameter "token" is required.');
+		}
+		if (is_string($token) === false) {
+			throw new \InvalidArgumentException(sprintf('Parameter "token" must be string, but type "%s" given.', get_debug_type($token)));
+		}
+
+		return $this->strategy->verify($token);
 	}
 }
